e4593d2138173be903aaf547e89c17e0f7e2749c
sujin <sjhur0417@gmail.com>
2022-06-15 16:24:42 +0900

    add month, year page

--- btnapp/visstep/urls.py
+++ btnapp/visstep/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('month/', views.month, name='month'),
+    path('month/<int:num>/', views.month, name='month'),
     path('year/', views.year, name='year'),
+    path('year/<int:num>/', views.year, name='year'),
+    path('specify/', views.specify, name='specify'),
+    path('compare/', views.compare, name='compare'),
 ]
--- btnapp/visstep/views.py
+++ btnapp/visstep/views.py
@@ -5,6 +5,9 @@ from django.core.paginator import Paginator, EmptyPage
 
 import datetime
 from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+from pandas import date_range
 
 
 from .models import StepCount_Data
@@ -12,83 +15,145 @@ from .models import StepCount_Data
 # Create your views here.
 
 def home(request):
+    # 이번주 날짜
     current = datetime.date.today()
     weekday = datetime.datetime.today().weekday()
     startday = current - timedelta(days=weekday)
-
+    # 이번주 DB id
     current_index = StepCount_Data.objects.get(saved_time = current).id
     startday_index = StepCount_Data.objects.get(saved_time = startday).id
     model_index = StepCount_Data.objects.latest('id').id
 
-
+    # pagination
     page = request.GET.get("page", 1)
-
+    # 이번주 데이터만 보여주기 위함
     if page == 1:
         datalist = StepCount_Data.objects.order_by('-saved_time')[model_index - current_index:model_index - startday_index +1]
         paginator = Paginator(datalist, current_index - startday_index + 1)
-        
+    # 저번주 부터 1주일씩 데이터 보여줌  
     else:
         datalist = StepCount_Data.objects.order_by('-saved_time')[model_index - startday_index - 6:]
         paginator = Paginator(datalist, 7)
-
+    # url에 있는 현재 page값 get_page로 전달
     data = paginator.page(page)  
  
+    # 날짜 범위 
     month = []
     day = []
     for i in reversed(data.object_list):
         month.append(i.saved_time.month)
         day.append(i.saved_time.day)
 
-    date = str(month[0]) + "월 " + str(day[0]) + "일 ~" + str(month[-1]) + "월 " + str(day[-1]) + "일"
+        date_range = str(month[0]) + "월 " + str(day[0]) + "일 ~" + str(month[-1]) + "월 " + str(day[-1]) + "일"
 
     context = {
         "page" : data,
         "page_list" : reversed(data.object_list),
-        "date" : date,
+        "date_range" : date_range,
     }
     return render(request, 'visstep/home.html', context)
 
-# def home(request):
-#     current = datetime.date.today()
-#     weekday = datetime.datetime.today().weekday()
-#     print(weekday)
-#     startday = current - timedelta(days=weekday)
-#     print(startday)
 
-#     dataset = StepCount_Data.objects.raw("SELECT * FROM stepcountData WHERE saved_time BETWEEN '" + str(startday) + "' and '" + str(current) +"'")
-#     # dataset = StepCount_Data.objects.raw("SELECT * FROM stepcountData WHERE saved_time BETWEEN '" + str(startday- timedelta(days=7)) + "' and '" + str(startday- timedelta(days=1)) +"'")
-
-#     date = []
-#     stepcount = []
-#     for i in dataset:
-#         date.append(i)
-#         stepcount.append(i.stepCount)
-#     print(type(date[0]))
-
-#     day = str(startday.month) + "월 " + str(startday.day) + "일 ~ " + str(current.month) + "월 " + str(current.day) + "일"
-     
-#     context = {
-#         'date': date,
-#         'stepcount' : stepcount,
-#         'day': day
-#     }
-
-#     return render(request, 'visstep/home.html', context)
-
-def month(request):
+def month(request, num = 1):
+    # 이번달 날짜
     current = datetime.date.today()
-    current_day = datetime.datetime.today().day
+    current_day = current.day
     startday = current - timedelta(days=current_day -1)
-    print(startday)
 
-    current_index = StepCount_Data.objects.get(saved_time = current).id
-    startday_index = StepCount_Data.objects.get(saved_time = startday).id
-    model_index = StepCount_Data.objects.latest('id').id
+    # next, prev page 번호 및 이에 따른 데이터 불러오기
+    next_page_index = num + 1
+    if num <= 1:
+        prev_page_index = 1
+        date = StepCount_Data.objects.filter(saved_time__range=[startday, current])
+  
+    else:
+        prev_page_index = num - 1
+        if current.month - num + 1 <= 0:
+            year_num = (current.month - num + 1)//(-12)
+            month_num = -((current.month - num + 1)%(-12))
+            date = StepCount_Data.objects.filter(saved_time__year = str(current.year - year_num - 1), saved_time__month = str(12 - month_num))
+            if StepCount_Data.objects.get(saved_time = str(date[0])).id == 1:
+                next_page_index = num
+        else:
+            date = StepCount_Data.objects.filter(saved_time__year = str(current.year), saved_time__month = str(current.month - num + 1))
+        
+    # 날짜 범위
+    first_date = StepCount_Data.objects.get(saved_time = str(date[0])).saved_time
+    date_range = str(first_date.year) + "년 " + str(first_date.month) + "월" 
 
     context = {
-
+        "date" : date,
+        "next_page_index" : next_page_index,
+        "prev_page_index" : prev_page_index,
+        "date_range" : date_range,
     }
     return render(request, 'visstep/month.html', context)
 
-def year(request):
-    return render(request, 'visstep/year.html')
+
+def year(request, num = 1):
+    # 올해 날짜
+    current = datetime.date.today()
+    current_month = current.month
+    current_day = current.day
+    startday = current - relativedelta(months=current_month -1) - timedelta(days=current_day -1)
+
+    # next, prev page 번호 및 이에 따른 데이터 불러오기
+    next_page_index = num + 1
+    if num <= 1:
+        prev_page_index = 1
+        date = StepCount_Data.objects.filter(saved_time__range=[startday, current])
+    else:
+        prev_page_index = num - 1
+        new_day = startday - relativedelta(years=num -1)
+        date = StepCount_Data.objects.filter(saved_time__range=[new_day, new_day + relativedelta(years=1) - timedelta(days=1)])
+        if StepCount_Data.objects.get(saved_time = str(date[0])).id == 1:
+            next_page_index = num
+
+    # 날짜 범위
+    first_date = StepCount_Data.objects.get(saved_time = str(date[0])).saved_time
+    date_range = str(first_date.year) + "년"
+
+
+    context = {
+        "date" : date,
+        "next_page_index" : next_page_index,
+        "prev_page_index" : prev_page_index,
+        "date_range" : date_range,
+    }
+    return render(request, 'visstep/year.html', context)
+
+def specify(request):
+    current = datetime.date.today()
+    weekday = datetime.datetime.today().weekday()
+    startday = current - timedelta(days=weekday)
+
+    date = StepCount_Data.objects.filter(saved_time__range = [startday, current])
+
+
+    date_range = str(startday.month) + "월 " + str(startday.day) + "일 ~ " + str(current.month) + "월 " + str(current.day) + "일"
+    print(date[0])
+
+    context = {
+        "date_range" :date_range,
+        "date" : date,
+    }
+    return render(request, 'visstep/specify.html', context)
+
+def compare(request):
+    current = datetime.date.today()
+    weekday = datetime.datetime.today().weekday()
+    startday = current - timedelta(days=weekday)
+
+    date_1 = StepCount_Data.objects.filter(saved_time__range = [startday, current])
+    date_2 = StepCount_Data.objects.filter(saved_time__range = [startday - timedelta(days= 7), startday - timedelta(days= 1)])
+
+
+    date_range = str(startday.month) + "월 " + str(startday.day) + "일 ~ " + str(current.month) + "월 " + str(current.day) + "일"
+
+
+    context = {
+        "date_range" :date_range,
+        "date_1" : date_1,
+        "date_2" : date_2,
+    }
+    return render(request, 'visstep/compare.html', context)
