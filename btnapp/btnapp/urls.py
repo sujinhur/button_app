db2ccb985c48eeb5da000dd376d1af24112caf7b
sujinhur <sjhur0417@gmail.com>
2022-06-21 23:40:23 +0900

    add code

--- btnapp/btnapp/urls.py
+++ btnapp/btnapp/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('visstep.urls')),
+    path('chat/', include('chatbot.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ btnapp/static/js/specify.js
@@ -0,0 +1,69 @@
+function specify(days, new_stepcount) {
+
+
+console.log(days);
+console.log(new_stepcount);
+
+var data = []
+
+for(var i = 0; i < days.length; i++) {
+    
+    data.push({'name': days[i], 'value': stepcount[i], color: '#add7a8'});
+    
+}
+
+console.log(data);
+
+var width = 300;
+var height = 270;
+var margin = {top: 40, left: 40, bottom: 40, right: 5};
+
+var x = d3.scaleBand()
+  .domain(data.map(d => d.name))
+  .range([margin.left, width - margin.right])
+  .padding(0.4);
+
+  var y = d3.scaleLinear()
+  .domain([0, 13000]).nice()
+  .range([height - margin.bottom, margin.top]);
+
+  var xAxis = g => g
+  .attr('transform', `translate(0, ${height - margin.bottom})`)
+  .call(d3.axisBottom(x)
+    .tickSizeOuter(0)
+    .tickFormat(g => g%2==1 ? g : null))
+  .call(g => g.select('.domain').remove())
+  .call(g => g.selectAll('line').remove());
+
+  var yAxis = g => g
+  .attr('transform', `translate(${margin.left}, 0)`)
+  .call(d3.axisLeft(y)
+    .ticks(6))
+  .call(g => g.select('.domain').remove())
+  .call(g => g.selectAll('line')
+    .attr('x2', width)
+    .style('stroke', '#f5f5f5')
+    .style('stroke-width', 2))
+  
+   
+    var svg = d3.select('#vis').append('svg').style('width', width).style('height', height);
+
+
+svg.append('g').call(xAxis).style("font-size", "10px");
+svg.append('g').call(yAxis).style("font-size", "9px");
+
+svg.append('g')
+    .selectAll('rect').data(data).enter().append('rect')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.value))
+    .attr('height', d => y(0) - y(d.value))
+    .attr("rx", 5)
+    .attr('width', x.bandwidth())
+    .attr('fill', d => d.color)
+    .attr('data-x', d => d.name)
+    .attr('data-y', d => d.value)
+    .attr('data-color', d=> d.color);
+
+svg.node();
+
+}
